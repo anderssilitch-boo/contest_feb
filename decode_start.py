91591ad22765a93b7ec893758f62b133a8e3c832
anderssilitch <anders.silitch@gmail.com>
2026-02-17 17:11:12 +0100

    commit

--- decode_start.py
+++ decode_start.py
@@ -3,4 +3,14 @@ vowel = "aeiou"
 message = ""
 with open("secret.txt") as f:
     for line in f:
-        print(line) # instead of print, you should start decoding
+         # instead of print, you should start decoding
+        # count vowels
+        # index the alfabet via the vowels count to get the letter
+        # collect all letters
+
+        num_vowels = 0
+        for v in vowel:
+            num_vowels = num_vowels + line.count(v)
+        letter = alphabet[num_vowels]
+        message = message + letter
+    print(message)
